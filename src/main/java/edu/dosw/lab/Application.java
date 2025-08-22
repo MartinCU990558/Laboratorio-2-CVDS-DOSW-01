00ac0d0aa5e93a743ac8bca8f8a1f0eb5922ddba
Julian <julianl116910@gmail.com>
2025-08-21 19:15:44 -0500

    feature:Preparacion - Subir Readme del Laboratorio 02

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,4 +1,4 @@
-package main.java.edu.dosw.lab;
+package edu.dosw.lab;
 public class Application {
     public static void main(String[] args){
         System.out.println("Proyecto Maven configurado y corriendo correctamente.");
