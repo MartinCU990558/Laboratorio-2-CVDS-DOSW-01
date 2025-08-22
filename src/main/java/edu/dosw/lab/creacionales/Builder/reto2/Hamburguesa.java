9490a2af543edf18eb1b9d3fce774bc5fa3f3437
unknown <juan.lcruz@labinfo.is.escuelaing.edu.co>
2025-08-22 15:49:04 -0500

    feature:Lista de ingredientes para tener guardados

--- src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Hamburguesa.java
+++
@@ -1,9 +0,0 @@
-package edu.dosw.lab.creacionales.builder.reto2;
-
-public class Hambuerguesa{
-    private ArrayList<String> listaIngredientes;
-
-    public Hambuerguesa(ArrayList<String> listaIngredientes) {
-        this.listaIngredientes = listaIngredientes;
-    }
-}
