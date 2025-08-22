11b71467db33b9d8a6159a21adce59925aa8c7c1
unknown <juan.rangel-r@mail.escuelaing.edu.co>
2025-08-22 15:51:42 -0500

    Implementacion de la clase ingrediente

--- Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/creacionales/Reto2/Ingrediente.java
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/creacionales/Reto2/Ingrediente.java
@@ -1,5 +1,17 @@
 package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01.creacionales.Reto2;
 
 public class Ingrediente {
-    
+    private String nombre;
+    private Int precio;
+
+    public ingrediente(String nombre,Int precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public int getprecio(){
+        return precio;
+    }
 }
