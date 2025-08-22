7c2bc317b1390e9cc5ce2ebaf90584f9e3b2d21e
unknown <samuel.albarracin-v@labinfo.is.escuelaing.edu.co>
2025-08-22 15:23:51 -0500

    ajustes

--- src/main/java/edu/dosw/lab/CarritoDeCompras.java
+++ src/main/java/edu/dosw/lab/CarritoDeCompras.java
@@ -3,10 +3,9 @@ package edu.dosw.lab;
 import java.util.ArrayList;
 public class CarritoDeCompras{
     private ArrayList <Producto> productos;
+
+
     public double total(){
-        return productos.stream().forEach(e -> e.getPrecio()).mapToDouble(Double::doubleValue()).sum();
-    }
-    public void añadiendoProductos(Producto producto){
-        productos.add(producto);
+        return productos.stream().mapToDobule(Double::doubleValue()).sum();
     }
 }
