ab006a76bef80ded496be7c56486953d29db80c7
unknown <david.palacios-p@mail.escuelaing.edu.co>
2025-08-22 14:32:34 -0500

    Feature trabajo inicial grupo 7

--- src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
+++ src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Laboratorio2CvdsDosw01Application {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Laboratorio2CvdsDosw01Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
