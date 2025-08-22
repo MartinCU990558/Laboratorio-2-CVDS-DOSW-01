7427f4131268e9d18083da139b3e541f04bb4eee
SantiagoSu15 <sasupu08@gmail.com>
2025-08-22 00:37:23 -0500

    feature: Preparacion - Subir Estructura de Paquetes del Laboratorio02

---
+++ src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01v/Laboratorio2CvdsDosw01vApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio2CvdsDosw01vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio2CvdsDosw01vApplication.class, args);
+	}
+
+}
