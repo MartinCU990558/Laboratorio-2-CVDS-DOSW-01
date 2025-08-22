8f25646dbbbd4ed39ae623f4dfc92d818cf4a9a3
Sergio Idarraga <chechotorres498@gmail.com>
2025-08-22 16:03:51 -0500

    Finalización del reto #1 Tienda don Pepe, ejecutandolo directamente en Aplocation

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,7 +1,14 @@
 package edu.dosw.lab;
+import edu.dosw.lab.solid.Reto_1_TIENDA_DON_PEPE.Tienda_Don_Pepe;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo");
+
+        Tienda_Don_Pepe tienda = new Tienda_Don_Pepe();
+        tienda.ejecutar();
     }
+
+    
+
 }
