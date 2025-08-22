3c7a8df212de254c4df217c48613cf246c400bc6
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-08-22 15:56:15 -0500

    Se añadió el main para probar el reto #2

---
+++ src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Compra.java
@@ -0,0 +1,15 @@
+package edu.dosw.lab.creacionales.Builder.reto2;
+import java.util.Map;
+
+public class Compra {
+    public static Map<String, Double> menu;
+    public static void main(String[] args) {
+        menu = menuIngredientes.obtenerIngredientes();
+        System.out.println("Los ingredientes disponibles son los siguientes:");
+        for (String i : menu.keySet()) {
+            Ingrediente ingrediente = new Ingrediente(i, menu.get(i));
+            System.out.println(ingrediente);
+        }
+    }
+
+}
