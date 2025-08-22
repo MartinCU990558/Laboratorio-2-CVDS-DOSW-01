36137b664f64c8d1d1313c3e8f99bd9af3f0453a
Diego Chavarro <diego.chavarro-c@mail.escuelaing.edu.co>
2025-08-22 15:32:58 -0500

    creacion de la clase ingrediente

--- src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Ingrediente.java
+++ src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Ingrediente.java
@@ -1,5 +1,24 @@
-package edu.dosw.lab.creacionales.builder.reto2;
+package edu.dosw.lab.creacionales.Builder.reto2;
 
-public class Ingrediente{
+class Ingrediente {
+    private String nombre;
+    private double precio;
 
+    public Ingrediente(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " ($" + precio + ")";
+    }
 }
