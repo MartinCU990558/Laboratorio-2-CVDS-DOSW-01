a41fee8c50caba50126a021a7a553e4a3ba79f40
daniel-pm19 <daniel.patino-m@mail.escuelaing.edu.co>
2025-08-21 19:27:26 -0500

    feature: Preparacion - Subir Estructura de Paquetes del Laboratorio 02

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
@@ -0,0 +1,7 @@
+package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01;
+
+public class Application {
+    public static void main(String[] args) {
+       System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Laboratorio-2-CVDS-DOSW-01
