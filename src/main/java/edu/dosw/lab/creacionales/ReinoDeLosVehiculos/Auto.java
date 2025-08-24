ba50f020842a219096acf8a3f3b89ce75742cabd
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-23 23:24:25 -0500

    clase auto subida

---
+++ src/main/java/edu/dosw/lab/creacionales/ReinoDeLosVehiculos/Auto.java
@@ -0,0 +1,32 @@
+package Reto3;
+
+public class Auto extends Vehiculo {
+
+    public Auto(String categoria) {
+
+        this.tipo = TipoVehiculo.TIERRA;
+        this.modelo = "Auto";
+        this.categoria = categoria;
+        configurar();
+    }
+
+    @Override
+    public void configurar() {
+
+        if (categoria.equals(Categoria.ECONOMICO)) {
+            this.velocidadMax = 120;
+            this.precio = 30000000;
+            this.equipamiento = "Básico";
+
+        } else if (categoria.equals(Categoria.LUJO)) {
+            this.velocidadMax = 180;
+            this.precio = 50000000;
+            this.equipamiento = "Aire acondicionado + GPS";
+
+        } else if (categoria.equals(Categoria.USADO)) {
+            this.velocidadMax = 100;
+            this.precio = 15000000;
+            this.equipamiento = "Equipamiento estándar";
+        }
+    }
+}
---
+++ src/main/java/edu/dosw/lab/creacionales/ReinoDeLosVehiculos/ComprarVehiculo.java
@@ -0,0 +1,21 @@
+package Reto3;
+import java.util.List;
+
+public class ComprarVehiculo {
+
+    public static void imprimirResumen(List<Vehiculo> vehiculos) {
+
+        System.out.println("---- RESUMEN DE COMPRA ---");
+
+        for (int i = 0; i < vehiculos.size(); i++) {
+            Vehiculo v = vehiculos.get(i);
+            System.out.printf("Vehículo %d:\nModelo: %s\nCategoría: %s\nVelocidad máxima: %d km/h\nPrecio: $%,.0f\nEquipamiento: %s\n\n",
+                    i+1, v.getModelo(), v.getCategoria(), v.getVelocidadMax(), v.getPrecio(), v.getEquipamiento());
+        }
+
+        double subtotal = vehiculos.stream().mapToDouble(Vehiculo::getPrecio).sum();
+        System.out.println("Subtotal: $" + subtotal);
+        System.out.println("Descuento aplicado: $0");
+        System.out.println("Total a pagar: $" + subtotal);
+    }
+}
