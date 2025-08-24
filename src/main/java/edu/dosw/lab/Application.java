e824be08cabe337ccf105769c9ff8ecc7d5c3382
carlos astudillo <carlos.astudillo-c@mail.escuelaing.edu.co>
2025-08-23 23:20:10 -0500

    creacion clase Fabrica y TipoVehiculo

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -25,9 +25,7 @@ public class Application {
         hamburguesas.ejecutar();
 
 
-
     }
-
 
 
 }
---
+++ src/main/java/edu/dosw/lab/creacionales/ReinoDeLosVehiculos/TipoVehiculo.java
@@ -0,0 +1,9 @@
+package edu.dosw.lab.creacionales.ReinoDeLosVehiculos;
+
+
+    public class TipoVehiculo {
+        public static final String TIERRA = "Tierra";
+        public static final String ACUATICO = "Acuatico";
+        public static final String AEREO = "Aereo";
+    }
+
