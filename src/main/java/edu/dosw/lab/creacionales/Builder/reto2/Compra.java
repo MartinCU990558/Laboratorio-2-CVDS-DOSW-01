0f3c6c47b8d414bc3b3f6e2b6aa04448f3c48a44
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-08-25 21:35:04 -0500

    Se eliminaron las clases Ingrediente.java y menuIngredientes.java debido a que no eran necesarias para la implementación del ejercicio.
    Ahora los ingredientes se manejan en un HashMap y en caso de necesitar nuevos ingredientes, simplemente se añaden mediante el metodo Builder.agregarIngrediente().
    
    Ahora el Builder se maneja dentro de la clase Hamburguesa para un mayor encapsulamiento y evitar que se le de un mal uso al builder.
    
    Se corrigieron errores y se optimizó el menu en la clase Compra.java

--- src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Compra.java
+++ src/main/java/edu/dosw/lab/creacionales/Builder/reto2/Compra.java
@@ -1,16 +1,69 @@
 package edu.dosw.lab.creacionales.Builder.reto2;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 public class Compra {
-    public static Map<String, Double> menu;
+    public static LinkedHashMap<String, Integer> menu = new LinkedHashMap<>() {{
+        put("Pan", 3000);
+        put("Carne", 10000);
+        put("Queso", 5000);
+        put("Lechuga", 2000);
+        put("Tomate", 2000);
+        put("Salsa especial", 3000);
+    }};
+
+    public static Hamburguesa burger;
+    private static Hamburguesa.Builder builder = new Hamburguesa.Builder();
+
     public static void main(String[] args) {
-        menu = menuIngredientes.obtenerIngredientes();
+        mostrarMenu();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Seleccione ingredientes para su hamburguesa: ");
+        String[] opciones = scanner.nextLine().split(",");
+        for (String op : opciones) {
+            if(Integer.parseInt(op) <= menu.size()){
+                ArrayList<String> claves = new ArrayList<>(menu.keySet());
+                String nombreIngrediente = claves.get(Integer.parseInt(op)-1);
+                Double precioIngrediente = menu.get(nombreIngrediente).doubleValue();
+                builder.agregarIngrediente(nombreIngrediente, precioIngrediente);
+            }else if(opciones.length == 0){
+                System.out.println("Ingrediente no encontrado");
+            }else{
+                añadirNuevoIngrediente();
+            }
+        }
+        System.out.println("\n----HAMBURGUESA PERSONALIZADA----");
+        Hamburguesa hamburguesa = builder.build();
+        System.out.println(hamburguesa);
+        System.out.println("---------------------------------");
+        System.out.println("Disfruta tu hamburguesa!");
+        scanner.close();
+    }
+
+    private static void mostrarMenu() {
         System.out.println("Los ingredientes disponibles son los siguientes:");
+
         int contador = 1;
         for (String i : menu.keySet()) {
-            Ingrediente ingrediente = new Ingrediente(i, menu.get(i));
-            System.out.println(contador+". " + ingrediente);
+            System.out.println(contador+". " + i + " ($" + menu.get(i) + ")");
             contador++;
         }
+        // System.out.println(contador+". " + "Añade un nuevo ingrediente");
+    }
+
+    private static void añadirNuevoIngrediente(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un nuevo ingrediente: ");
+        String nuevoIngre = scanner.nextLine();
+        System.out.print("Ingrese el valor del ingrediente: ");
+        double precioIngre = scanner.nextInt();
+        builder.agregarIngrediente(nuevoIngre, precioIngre);
+        scanner.close();
+
+        
+
     }
 }
