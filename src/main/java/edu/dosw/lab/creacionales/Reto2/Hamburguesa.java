54b88fbb47d84cb96fc070b1198f33eac875407c
SantiagoSu15 <sasupu08@gmail.com>
2025-08-26 09:36:11 -0500

    Actualizacion Reto 2

--- src/main/java/edu/dosw/lab/creacionales/Reto2/Hamburguesa.java
+++ src/main/java/edu/dosw/lab/creacionales/Reto2/Hamburguesa.java
@@ -6,7 +6,24 @@ public class Hamburguesa {
     private ArrayList<Ingrediente> ingredientes;
 
     public Hamburguesa(ArrayList<Ingrediente> ingredientes) {
-        this.ingredientes = ingredientes;
+        this.ingredientes = new ArrayList<>(ingredientes);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hamburguesa{ingredientes=");
+        ingredientes.forEach(p -> sb.append(p.getNombre()).append(" ").append(p.getPrecio()).append(", "));
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public double getPrecio() {
+        double total = ingredientes
+                .stream()
+                .mapToDouble(Ingrediente::getPrecio)
+                .sum();
+        return total;
     }
 
 }
--- src/main/java/edu/dosw/lab/creacionales/Reto2/Pedido.java
+++ src/main/java/edu/dosw/lab/creacionales/Reto2/Pedido.java
@@ -8,22 +8,16 @@ import java.util.stream.IntStream;
 
 public class Pedido {
     private ArrayList<Ingrediente> ingredientes_disponibles;
-    public ArrayList<Ingrediente> ingredientes_pedidos;
+    private ArrayList<Ingrediente> ingredientes_pedidos;
+    private Cocina cocina;
 
-    public Pedido() {
-        this.ingredientes_disponibles = new ArrayList<>();
+    public Pedido(Cocina cocina) {
+        this.cocina = cocina;
+        this.ingredientes_disponibles = this.cocina.getIngredientes_disponibles();
         this.ingredientes_pedidos = new ArrayList<>();
-        setIngredientesDisponibles();
     }
 
-    private void setIngredientesDisponibles() {
-        ingredientes_disponibles.add(new Carne());
-        ingredientes_disponibles.add(new Lechuga());
-        ingredientes_disponibles.add(new Pan());
-        ingredientes_disponibles.add(new Queso());
-        ingredientes_disponibles.add(new Salsa());
-        ingredientes_disponibles.add(new Tomate());
-    }
+
 
     public ArrayList<Ingrediente> getIngredientes_disponibles() {
         return ingredientes_disponibles;
@@ -47,7 +41,7 @@ public class Pedido {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingresa el nuevo ingrediente: ");
         String nome = sc.nextLine();
-        System.out.print("Ingresa el nuevo valor: ");
+        System.out.print("Ingresa el nuevo valor (Double->0,0): ");
         double valor = sc.nextDouble();
         Nuevo n = new Nuevo(nome,valor);
         ingredientes_pedidos.add(n);
@@ -62,37 +56,11 @@ public class Pedido {
 
     }
 
-
-    public static void main(String[] args) {
-        Pedido pedido = new Pedido();
-        Scanner sc = new Scanner(System.in);
-
-        ArrayList<Ingrediente> s = pedido.getIngredientes_disponibles();
-
-        System.out.println("Seleccione Los Ingredientes");
-        IntStream.range(0, s.size())
-                .forEach(i -> System.out.println(i+1 + ": " + s.get(i).getNombre() + "(" + s.get(i).getPrecio() + ")"));
-        System.out.println("7: Agregar nuevo Ingrediente" );
-        System.out.print("Ingresa números separados por coma: ");
-        String input = sc.nextLine();
-        String[] partes = input.split(",");
-
-        pedido.agregarIngredientes(partes);
-        System.out.println("--------------Hamburguesa--------");
-        System.out.println("Ingredientes:");
-        pedido.ingredientes_pedidos.forEach(p -> System.out.println(p.getNombre() + " " + p.getPrecio()));
-        double total = pedido.ingredientes_pedidos
-                .stream()
-                .mapToDouble(Ingrediente::getPrecio)
-                .sum();
-        System.out.println("Precio: " + total);
-        System.out.println("------------------------------");
-        System.out.println("Disfrute");
-
-
-
-
-
+    public ArrayList<Ingrediente> obtenerPedido(){
+        return ingredientes_pedidos;
     }
+
+
+
 
 }
