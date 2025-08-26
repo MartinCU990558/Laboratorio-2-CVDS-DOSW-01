7b779f3341788f9d8fbdbcca9be36319bad89178
nicolasduarter <nicolas.duarte-r@mail.escuelaing.edu.co>
2025-08-26 12:16:21 -0500

    Update Application.java

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,7 +1,10 @@
 package edu.dosw.lab;
 
+import edu.dosw.lab.creacionales.reto2.Reto2;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("™ Proyecto Maven configurado y corriendo correctamente");
+        Reto2.run();
     }
 }
