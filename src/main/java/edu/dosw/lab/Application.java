ac99d818d745019f64f8b2ec23a94d228e090a9e
Tulio <tuliojr3101@gmail.com>
2025-08-26 19:07:38 -0500

    Correcion Reto 2

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -8,7 +8,7 @@ public class Application {
     public static void main(String args[]) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
         // Reto1TiendaDonPepe.ejecutar();
-        // Reto2ChefCincoEstrellas.ejecutar();
-        Reto5CafePersonalizado.ejecutar();
+        Reto2ChefCincoEstrellas.ejecutar();
+        // Reto5CafePersonalizado.ejecutar();
     }
 }
