9b40f12cb7624966c516a991c518d76f80820e43
Juan Leal <lecrujuan@gmail.com>
2025-08-28 18:17:07 -0500

    feature:Se crean las carpetas pertinentes para poder realizar el reto 5. Se adjuntan las clases ToppingDecorator y ToppingsCafe

---
+++ src/main/java/edu/dosw/lab/Estructurales/Decorator/reto5/ToppingsCafe.java
@@ -0,0 +1,21 @@
+package edu.dosw.lab.Estructurales.Decorator.reto5;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ToppingsCafe {
+    private static final HashMap<String, Integer> toppings = new LinkedHashMap<>();
+
+    static {
+        toppings.put("Leche",1000);
+        toppings.put("Chocolate",1500);
+        toppings.put("Caramelo",1200);
+        toppings.put("Crema batida",2000);
+        toppings.put("Menta",1300);
+    }
+
+    public static HashMap<String, Integer> obtenerToppings() {
+        return toppings;
+    }
+}
