2dbd5958397766b14e7228e76b57ddc1e7c419df
Sergio Idarraga <chechotorres498@gmail.com>
2025-08-27 22:00:20 -0500

    Refactor del reto 4 debido a que se guardó en un paquete que no era, además se implementó la clase  Cafe

---
+++ src/main/java/edu/dosw/lab/estructurales/CafePersonalizado/Cafe.java
@@ -0,0 +1,35 @@
+package edu.dosw.lab.estructurales.CafePersonalizado;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cafe {
+    private CafeBase base;
+    private List<Topping> toppings;
+
+    public Cafe() {
+        this.base = new CafeBase();
+        this.toppings = new ArrayList<>();
+    }
+
+    public void agregarTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public String getDescripcion() {
+        StringBuilder desc = new StringBuilder(base.getDescripcion());
+        for (Topping t : toppings) {
+            desc.append(" + ").append(t.getNombre());
+        }
+        return desc.toString();
+    }
+
+    public double getPrecio() {
+        double total = base.getPrecio();
+        for (Topping t : toppings) {
+            total += t.getPrecio();
+        }
+        return total;
+    }
+}
