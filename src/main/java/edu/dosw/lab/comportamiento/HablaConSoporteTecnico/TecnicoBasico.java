e351537ca613315036aa6b838dfbf330efafced6
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-27 22:19:49 -0500

    clase tecnico basico y intermedio

--- src/main/java/edu/dosw/lab/comportamiento/HablaConSoporteTecnico/TecnicoBasico.java
+++ src/main/java/edu/dosw/lab/comportamiento/HablaConSoporteTecnico/TecnicoBasico.java
@@ -6,6 +6,18 @@ public class TecnicoBasico extends Tecnico {
         super("Basico");
     }
 
-
+    @Override
+    public boolean manejarTicket(Ticket ticket) {
+        if (ticket.getNivelComplejidad().equals("Basico")) {
+            System.out.println("Técnico Básico manejando ticket: " + ticket.getDescripcionProblema() + " con prioridad " + ticket.getNivelPrioridad());
+            return true;
+        } else if (siguienteTecnico != null) {
+            System.out.println("Técnico Básico escalando ticket: " + ticket.getDescripcionProblema() + " a técnico " + siguienteTecnico.nivelSoporte);
+            return siguienteTecnico.manejarTicket(ticket);
+        } else {
+            System.out.println("Ningún técnico disponible. Ticket pendiente de escalamineto. " + ticket.getDescripcionProblema());
+            return false;
+        }
+    }
 
 }
--- src/main/java/edu/dosw/lab/comportamiento/HablaConSoporteTecnico/TecnicoIntermedio.java
+++ src/main/java/edu/dosw/lab/comportamiento/HablaConSoporteTecnico/TecnicoIntermedio.java
@@ -1,7 +1,17 @@
 package edu.dosw.lab.comportamiento.HablaConSoporteTecnico;
 
 public class TecnicoIntermedio extends Tecnico {
-
-
+    @Override
+    public boolean manejarTicket(Ticket ticket) {
+        if (ticket.getNivelComplejidad().equalsIgnoreCase("Intermedio")) {
+            System.out.println("El técnico de nivel " + nivelSoporte + " está manejando el ticket con prioridad " + ticket.getNivelPrioridad() + ": " + ticket.getDescripcionProblema());
+            return true;
+        } else if (siguienteTecnico != null) {
+            System.out.println("El técnico de nivel " + nivelSoporte + " no puede manejar el ticket con complejidad " + ticket.getNivelComplejidad() + ". Pasando al siguiente técnico.");
+            return siguienteTecnico.manejarTicket(ticket);
+        } else {
+            System.out.println("El ticket con complejidad " + ticket.getNivelComplejidad() + " no puede ser manejado. No hay más técnicos disponibles.");
+        } return false;
+    }
 
 }
