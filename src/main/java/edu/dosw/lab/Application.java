e02fde73ff3fe8303a9956f341f6fd6e895a6918
Juan Pablo Caballero Castellanos <juan.ccastellanos@mail.escuelaing.edu.co>
2025-08-27 21:35:14 -0500

    Reto 4 culminado

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -2,6 +2,7 @@ package edu.dosw.lab;
 import edu.dosw.lab.solid.Reto1.Reto1TiendaDonPepe;
 import edu.dosw.lab.creacionales.Reto2.ChefDe5Estrellas;
 import edu.dosw.lab.estructurales.Reto5.ElCafePerzonalizado;
+import edu.dosw.lab.estructurales.Reto4.CasaDeCambio;
 import edu.dosw.lab.comportamiento.Reto6.SoporteTecnico;
 public class Application {
     public static void main(String[] args){
@@ -9,6 +10,7 @@ public class Application {
         Reto1TiendaDonPepe.ejecutar1();
         ChefDe5Estrellas.ejecutar2();
         ElCafePerzonalizado.ejectutar5();
+        CasaDeCambio.ejecutar4();
         SoporteTecnico.ejecutar6();
     }
 }
