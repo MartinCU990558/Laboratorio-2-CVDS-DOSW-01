18c7f73f0150f044bcb9aecacc41279a7135c082
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-08-27 21:34:32 -0500

    empezando con el reto5 del cafe personalizado

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -3,13 +3,14 @@ import edu.dosw.lab.creacionales.ChefDeCincoEstrellasReto2.ChefDeCincoEstrellas;
 import edu.dosw.lab.creacionales.ReinoDeLosVehiculos.ReinoDeLosVehiculos;
 import edu.dosw.lab.solid.Reto1TiendaDonPepe.*;
 import edu.dosw.lab.comportamiento.CasaDeCambioReto4.CasaDeCambio;
+import edu.dosw.lab.estructurales.CafePersonalizado.CafePersonalizadoClase;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo");
 
-        /**
-        // Ejecución del reto 1 Tienda Don Pepe
+
+        /** Ejecución del reto 1 Tienda Don Pepe
         Producto camiseta = new Producto("Camiseta", 20000);
         Producto pantalon = new Producto("Pantalón", 50000);
         Producto galletas = new Producto("Galletas", 500);
@@ -24,18 +25,16 @@ public class Application {
 
         // Ejecución del reto 2
         ChefDeCincoEstrellas hamburguesas = new ChefDeCincoEstrellas();
-        hamburguesas.ejecutar();***/
+        hamburguesas.ejecutar();
 
         // Ejecución del reto 3 Reino de los Vehículos
-        ReinoDeLosVehiculos reinoVehiculos = new ReinoDeLosVehiculos();
-        reinoVehiculos.ejecutar();
+        /**ReinoDeLosVehiculos reinoVehiculos = new ReinoDeLosVehiculos();
+        reinoVehiculos.ejecutar();**/
 
-        /**
-         * // EJECUCION RETO4 casa de cambio
-        CasaDeCambio cambio = new CasaDeCambio();
+        // EJECUCION RETO4 casa de cambio
+
+        /**CasaDeCambio cambio = new CasaDeCambio();
         cambio.ejecutar();**/
-
-
 
     }
 
