4b27208086b7ee22d616fc396aa2d6f551b53248
Diego Chavarro <diego.chavarro-c@mail.escuelaing.edu.co>
2025-08-28 22:44:37 -0500

    ajuste de main, para cambiarlo a un metodo de ejecutar()

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,7 +1,10 @@
 package edu.dosw.lab;
+import edu.dosw.lab.comportamiento.Comand.reto7.ControlRemotoMagicoMain;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("Proyecto Maven configurado y corriendo completamente");
+        ControlRemotoMagicoMain reto7 = new ControlRemotoMagicoMain();
+        reto7.ejecutar();
     }
 }
--- src/main/java/edu/dosw/lab/comportamiento/Comand/reto7/ControlRemotoMagicoMain.java
+++ src/main/java/edu/dosw/lab/comportamiento/Comand/reto7/ControlRemotoMagicoMain.java
@@ -3,7 +3,7 @@ package edu.dosw.lab.comportamiento.Comand.reto7;
 import java.util.Scanner;
 
 public class ControlRemotoMagicoMain {
-    public static void main(String[] args) {
+    public void ejecutar() {
         Scanner scanner = new Scanner(System.in);
         ControlRemotoMagico control = new ControlRemotoMagico();
 
