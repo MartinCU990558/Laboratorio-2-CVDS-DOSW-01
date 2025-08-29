cc529200bf79e8c941b06351c736a847b1b954d4
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-08-28 23:49:11 -0500

    Se corrigieron importaciones de paquetes

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,21 +1,19 @@
 package edu.dosw.lab;
 
-import edu.dosw.lab.solid.reto1;
-import edu.dosw.lab.creacionales.Builder.reto2;
-import edu.dosw.lab.creacionales.FactoryMethod;
-import edu.dosw.lab.Estructurales.Adapter.reto4;
-import edu.dosw.lab.Estructurales.Decorator.reto5;
-import edu.dosw.lab.comportamiento.ChainOfResponsibility.reto6;
+import edu.dosw.lab.comportamiento.ChainOfResponsibility.reto6.Reto6;
+import edu.dosw.lab.creacionales.FactoryMethod.ReinoVehiculos;
+import edu.dosw.lab.solid.reto1.TiendaDonPepe;
+import edu.dosw.lab.creacionales.Builder.reto2.Compra;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("Proyecto Maven configurado y corriendo completamente");
 
         TiendaDonPepe.ejecutar();
-        Compra.ejectuar();
+        Compra.ejecutar();
         ReinoVehiculos.ejecutar();
-        CasaCambio.ejecutar();
-        TiendaCafe.ejecutar();
+        edu.dosw.lab.Estructurales.Adapter.reto4.CasaCambio.ejecutar();
+        edu.dosw.lab.Estructurales.Decorator.reto5.TiendaCafe.ejecutar();
         Reto6.ejecutar();
     }
 }
