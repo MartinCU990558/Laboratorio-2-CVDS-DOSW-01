773e5b3e6e141e96fe9e2de90917d26d4b8d8f29
Juan Leal <lecrujuan@gmail.com>
2025-08-28 23:44:54 -0500

    Actualización de Application para poder ejecutar los archivos

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,8 +1,21 @@
 package edu.dosw.lab;
 
+import edu.dosw.lab.solid.reto1;
+import edu.dosw.lab.creacionales.Builder.reto2;
+import edu.dosw.lab.creacionales.FactoryMethod;
+import edu.dosw.lab.Estructurales.Adapter.reto4;
+import edu.dosw.lab.Estructurales.Decorator.reto5;
+import edu.dosw.lab.comportamiento.ChainOfResponsibility.reto6;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Proyecto Maven configurado y corriendo completamente");
-    }
 
+        TiendaDonPepe.ejecutar();
+        Compra.ejectuar();
+        ReinoVehiculos.ejecutar();
+        CasaCambio.ejecutar();
+        TiendaCafe.ejecutar();
+        Reto6.ejecutar();
+    }
 }
