a7ad56ccfbb801e616e90bd3eaa7969513be6e93
Diego Chavarro <diego.chavarro-c@mail.escuelaing.edu.co>
2025-08-29 00:00:32 -0500

    Ajuste en la clase Appñication para evitar conflictos

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -4,7 +4,6 @@ import edu.dosw.lab.comportamiento.Comand.reto7.ControlRemotoMagicoMain;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Proyecto Maven configurado y corriendo completamente");
-        ControlRemotoMagicoMain reto7 = new ControlRemotoMagicoMain();
-        reto7.ejecutar();
+
     }
 }
